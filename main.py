c1fa077249118f0bd13fdcf300ad3b2a60a8512f
Егор <ebrexovskix@yandex.ru>
2021-11-19 16:26:56 +0300

    Задание 2 выполнено на 102%

--- main.py
+++ main.py
@@ -2,7 +2,114 @@ import sys
 import sqlite3
 
 from PyQt5 import uic  # Импортируем uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QInputDialog
+import traceback
+
+
+def excepthook(exc_type, exc_value, exc_tb):
+    tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+    print(tb)
+
+
+sys.excepthook = excepthook
+
+
+class changeDB(QDialog):
+    def __init__(self, parent):
+        super().__init__(parent)
+        uic.loadUi('addEditCoffeeForm.ui', self)  # Загружаем дизайн
+
+        names = ex.cur.execute(f"""SELECT name FROM coffee""").fetchall()
+
+        for i in names:
+            self.comboBox.addItem(i[0])
+
+        self.pushButton.clicked.connect(self.change)
+        self.pushButton_2.clicked.connect(self.new)
+
+        self.spinBox_2.setMaximum(10000)
+        self.update()
+        self.comboBox.currentIndexChanged.connect(lambda s: self.update())
+
+        self.spinBox.setMaximum(10000)
+        self.spinBox_3.setMaximum(10000)
+        self.spinBox_4.setMaximum(10000)
+
+        self.spinBox.setMinimum(10)
+        self.spinBox_3.setMinimum(10)
+        self.spinBox_4.setMinimum(10)
+
+    def change(self, _):
+        name = self.comboBox.currentText()
+
+        if self.comboBox_3.currentText() == 'Светлая':
+            d = 0
+        elif self.comboBox_3.currentText() == "Средняя":
+            d = 1
+        else:
+            d = 2
+
+        ex.cur.execute(f"""UPDATE coffee 
+        SET volume_x={self.spinBox.value()},
+        volume_y={self.spinBox_3.value()},
+        volume_z={self.spinBox_4.value()},
+        price = {self.spinBox_2.value()},
+        taste='{self.textEdit.toPlainText()}',
+        ground={self.comboBox_3.currentIndex()},
+        degreeOfRosting={self.comboBox_2.currentIndex()}
+        WHERE name='{name}'""")
+
+        ex.con.commit()
+        self.close()
+        ex.run(name)
+
+    def new(self, _):
+        name, ok_pressed = QInputDialog.getText(self, "Введите название кофе",
+                                                "Новый кофе")
+        if not ok_pressed:
+            return
+        ex.cur.execute(f"""INSERT INTO coffee (name)
+        VALUES('{name}')""")
+
+        ex.cur.execute(f"""UPDATE coffee 
+        SET volume_x={self.spinBox.value()},
+        volume_y={self.spinBox_3.value()},
+        volume_z={self.spinBox_4.value()},
+        price = {self.spinBox_2.value()},
+        taste='{self.textEdit.toPlainText()}',
+        ground={self.comboBox_3.currentIndex()},
+        degreeOfRosting={self.comboBox_2.currentIndex()}
+        WHERE name='{name}'""")
+
+        ex.con.commit()
+        self.close()
+        ex.run(name)
+
+        ex.comboBox.addItem(name)
+
+    def update(self):
+        name = self.comboBox.currentText()
+        parameters = ex.cur.execute(f"""SELECT * FROM coffee WHERE name = '{name}'""").fetchall()[0]
+
+        if parameters[2] == 0:
+            self.comboBox_2.setCurrentText('Светлая')
+        elif parameters[2] == 1:
+            self.comboBox_2.setCurrentText('Средняя')
+        else:
+            self.comboBox_2.setCurrentText('Темная')
+
+        if parameters[3]:
+            self.comboBox_3.setCurrentText('Молотый')
+        else:
+            self.comboBox_3.setCurrentText('В зернах')
+
+        self.textEdit.setText(parameters[4])
+
+        self.spinBox_2.setValue(parameters[5])
+
+        self.spinBox.setValue(parameters[6])
+        self.spinBox_3.setValue(parameters[7])
+        self.spinBox_4.setValue(parameters[8])
 
 
 class MyWidget(QMainWindow):
@@ -19,21 +126,21 @@ class MyWidget(QMainWindow):
         for i in names:
             self.comboBox.addItem(i[0])
 
-        self.comboBox.addItem('qwerty')
         self.run(names[0][0])
 
         self.comboBox.currentIndexChanged.connect(self.run)
 
+        self.pushButton.clicked.connect(self.openWindow)
+
     def run(self, name):
         name = self.comboBox.currentText()
         parameters = self.cur.execute(f"""SELECT * FROM coffee WHERE name = '{name}'""").fetchall()[0]
-        for i in parameters:
-            print(i)
+
         self.label_8.setText(parameters[1])
-        if parameters[1]:
-            self.label_3.setText('молотый')
+        if parameters[3]:
+            self.label_3.setText('Молотый')
         else:
-            self.label_3.setText('в зернах')
+            self.label_3.setText('В зернах')
 
         self.label_4.setText(parameters[4])
 
@@ -41,16 +148,19 @@ class MyWidget(QMainWindow):
 
         size = f'{parameters[6]}x{parameters[7]}x{parameters[8]}'
 
-        if parameters[3] == 0:
+        if parameters[2] == 0:
             self.label_7.setText('Светлая')
-        elif parameters[3] == 1:
+        elif parameters[2] == 1:
             self.label_7.setText('Средняя')
         else:
             self.label_7.setText('Темная')
 
-
         self.label_10.setText(size)
 
+    def openWindow(self):
+        window = changeDB(self)
+        print('done')
+        window.exec()
 
 
 
